f0eaee032fdac816d94802726b01579cabb387d6
William Rogge <williamroggeiv@gmail.com>
2020-05-09 20:41:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   dragon_ball = ["Goku"]
-  dragon_ball << "Vegeta"
+  dragon_ball << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
