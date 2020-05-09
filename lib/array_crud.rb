c5f4db8139624b0f077da8fc287fed6518dd5ced
William Rogge <williamroggeiv@gmail.com>
2020-05-09 21:04:56 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Goku", "Gohan", "Vegeta", "Bulma"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["Goku", "Gohan"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["Goku"]
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
+  array = ["Goku", "arrays!"]
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "Goku"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["am", "Goku"]
+  array [0]
 end
 
 def retrieve_first_element_from_array(array)
+  array = ["wow", "Goku"]
+  array.first
   
 end
 
 def retrieve_last_element_from_array(array)
+  array = ["Goku", "arrays!"]
+  array.last
   
 end
