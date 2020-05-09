a594089c791b677f669bd31cdec1409fbfa57649
William Rogge <williamroggeiv@gmail.com>
2020-05-09 21:02:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,8 +28,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["am"]
-  array = [0]
+  array = ["am", "Goku"]
+  array [0]
 end
 
 def retrieve_first_element_from_array(array)
